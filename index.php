a9f230cde5c66dd0a591aa50d483f2bfe106c3a9
AREZKI <arezki.m.nadji96@gmail.com>
2022-03-07 22:20:29 +0100

    First Try pagination

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Pagination</title>
+</head>
+<body>
+<?php
+// connect to database
+$con = mysqli_connect('localhost','root','Tnlvk0oIyc3wx8Qk');
+mysqli_select_db($con, 'pagination_paris_8');
+
+// Definir le nombre de résultat voulu par page
+$results_per_page = 10;
+
+// Nombre total de résultat dans la base de données
+$sql='SELECT * FROM pagination';
+$result = mysqli_query($con, $sql);
+$number_of_results = mysqli_num_rows($result);
+
+// Determiner le nombre de page affichabe
+$number_of_pages = ceil($number_of_results/$results_per_page);
+
+// Determiner sur quel page est l'utilisateur acctuelement
+if (!isset($_GET['page'])) {
+  $page = 1;
+} else {
+  $page = $_GET['page'];
+}
+
+// Determiner le nombre de résultat afficher par page
+$this_page_first_result = ($page-1)*$results_per_page;
+
+// Récupération des résultat dans la base de données 
+$sql='SELECT * FROM pagination LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
+$result = mysqli_query($con, $sql);
+
+while($row = mysqli_fetch_array($result)) {
+  echo $row['id'] . ' ' . $row['alphabet']. '<br>';
+}
+
+// Affichage des liens 
+for ($page=1;$page<=$number_of_pages;$page++) {
+  echo '<a href="index.php?page=' . $page . '">' . $page . '</a> ';
+}
+
+?>
+</body>
+</html>
---
+++ pagination.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 07 mars 2022 à 21:18
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pagination_paris_8`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pagination`
+--
+
+DROP TABLE IF EXISTS `pagination`;
+CREATE TABLE IF NOT EXISTS `pagination` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alphabet` varchar(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `pagination`
+--
+
+INSERT INTO `pagination` (`id`, `alphabet`) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D'),
+(5, 'E'),
+(6, 'F'),
+(7, 'G'),
+(8, 'H'),
+(9, 'I'),
+(10, 'J'),
+(11, 'K'),
+(12, 'L'),
+(13, 'M'),
+(14, 'N'),
+(15, 'O'),
+(16, 'P'),
+(17, 'Q'),
+(18, 'R'),
+(19, 'S'),
+(20, 'T'),
+(21, 'U'),
+(22, 'V'),
+(23, 'W'),
+(24, 'X'),
+(25, 'Y'),
+(26, 'Z');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
